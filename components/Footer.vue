d14b9f6008cca32771e863e8e9fc496b2a595488
埃拉 <ElaBosak233@qq.com>
2022-08-03 19:45:01 +0800

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="py-6">
         <div class="unselectable flex flex-col items-center">
-            <p class="text-xs text-gray-600">© <div class="font-bold inline">CC-BY-NC-SA 4.0</div> 2019-2022 埃拉</p>
+            <p class="text-xs text-gray-600">© CC-BY-NC-SA 4.0 2019-2022 埃拉</p>
         </div>
     </div>
 </template>
