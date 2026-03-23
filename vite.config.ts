70608e86d75f5c7b0337cc58d647d7e3d15afac6
Ela <i@e23.dev>
2026-03-23 11:26:47 +0800

    feat: nitro

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
@@ -15,6 +16,7 @@ const config = defineConfig({
         quoteStyle: "double",
       },
     }),
+    nitro(),
     viteReact(),
   ],
   resolve: {
